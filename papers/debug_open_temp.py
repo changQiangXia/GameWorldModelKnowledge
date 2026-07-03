a7da2e4d2605fc777102dec7386407331b5c7088
changQiangXia <2454212515@qq.com>
2026-07-03 10:58:41 +0800

    Delete some intermediate products.

--- papers/debug_open_temp.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-path = Path(r"D:\gameWorld\papers\DreamerV4\source.tar.gz.download")
-if path.exists():
-    path.unlink()
-with path.open("wb") as file:
-    file.write(b"ok")
-print(path, path.stat().st_size)
